170564f76a72ef7c40f1cb7eebb9d4fafac091d9
codinghaki <113092980+codinghaki@users.noreply.github.com>
2023-11-06 19:30:10 +1100

    Completed initial functions

---
+++ JS/app.js
@@ -0,0 +1,31 @@
+function add(operand1, operand2){
+    return operand1 + operand2;
+}
+
+function subtract(operand1, operand2){
+    return operand1 - operand2;
+}
+
+function multiply(operand1, operand2){
+    return operand1 * operand2;
+}
+
+function divide(operand1, operand2){
+    if (operand2 == 0){
+        return 'Cannot divide by 0';
+    }
+    return operand1 / operand2;
+}
+
+function operate(operand1, operand2, operatorASCII){
+    switch(operatorASCII){
+        case "+":
+            return add(operand1, operand2);
+        case "-":
+            return subtract(operand1, operand2);
+        case "*":
+            return multiply(operand1, operand2);
+        case "/":
+            return divide(operand1, operand2);
+    }
+}
